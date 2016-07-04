1ec0c738af3d9f4319731abc10e9f6eed6ce1ada
sunil kumar <snlkumar447@gmail.com>
2016-07-04 09:52:25 +0530

    add configuration

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -6,6 +6,12 @@ pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 listen '/tmp/unicorn.bookmycook.sock'
+before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end   
+end
 listen 8200
 worker_processes 2
 timeout 360
